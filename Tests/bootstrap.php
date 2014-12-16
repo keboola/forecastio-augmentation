d2cb859bd6448a2c2c99342df86c39e688a5ae4c
Jakub Matejka <jakub@keboola.com>
2014-12-16 09:06:41 +0000

    test: read environment variables

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -9,4 +9,26 @@ if (file_exists(__DIR__ . '/config.php')) {
 	require_once __DIR__ . '/config.php';
 }
 
+
+defined('STORAGE_API_URL')
+|| define('STORAGE_API_URL', getenv('STORAGE_API_URL') ? getenv('STORAGE_API_URL') : 'https://connection.keboola.com');
+
+defined('STORAGE_API_TOKEN')
+|| define('STORAGE_API_TOKEN', getenv('STORAGE_API_TOKEN') ? getenv('STORAGE_API_TOKEN') : 'your_token');
+
+defined('FORECASTIO_KEY')
+|| define('FORECASTIO_KEY', getenv('FORECASTIO_KEY') ? getenv('FORECASTIO_KEY') : 'your_forecastio_api_key');
+
+defined('DB_HOST')
+|| define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : '127.0.0.1');
+
+defined('DB_NAME')
+|| define('DB_NAME', getenv('DB_NAME') ? getenv('DB_NAME') : 'ag_forecastio');
+
+defined('DB_USER')
+|| define('DB_USER', getenv('DB_USER') ? getenv('DB_USER') : 'user');
+
+defined('DB_PASSWORD')
+|| define('DB_PASSWORD', getenv('DB_PASSWORD') ? getenv('DB_PASSWORD') : '');
+
 require_once __DIR__ . '/../vendor/autoload.php';
