adb4d208282427698e82af13da05a4d4b890969c
Jakub Matejka <jakub@keboola.com>
2016-06-13 12:24:14 +0200

    fix: codestyle check

--- tests.sh
+++ tests.sh
@@ -1,4 +1,4 @@
 php --version \
   && composer --version \
-  && ./vendor/bin/phpcs --standard=psr2 -n --ignore=vendor . \
+  && ./vendor/bin/phpcs --standard=psr2 -n --ignore=vendor,src/CNG . \
   && ./vendor/bin/phpunit
