82faab84acfb96de51ade4d104c4d5d58386bc1b
Martin Halamíček <martin@halamicek.cz>
2017-09-23 11:33:58 +0200

    deploy to dev portal FIXES #3

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,19 @@
-#!/bin/bash
+docker pull quay.io/keboola/developer-portal-cli-v2:latest
+export REPOSITORY=`docker run --rm \
+  -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME \
+  -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD \
+  quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola.ag-forecastio`
+docker tag keboola/forecastio-augmentation:latest $REPOSITORY:$TRAVIS_TAG
+docker tag keboola/forecastio-augmentation:latest $REPOSITORY:latest
+eval $(docker run --rm \
+  -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME \
+  -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD \
+  quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login keboola keboola.ag-forecastio)
+docker push $REPOSITORY:$TRAVIS_TAG
+docker push $REPOSITORY:latest
 
-docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-docker tag keboola/forecastio-augmentation quay.io/keboola/forecastio-augmentation:$TRAVIS_TAG
-docker images
-docker push quay.io/keboola/forecastio-augmentation:$TRAVIS_TAG
+docker run --rm \
+  -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME \
+  -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD \
+  quay.io/keboola/developer-portal-cli-v2:latest update-app-repository keboola keboola.ag-forecastio $TRAVIS_TAG
+
