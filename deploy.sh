62f0f003d254358ff6e9c8f6409828724de86e78
Martin Halamíček <martin@halamicek.cz>
2017-09-23 11:45:08 +0200

    fix - ecr deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ docker pull quay.io/keboola/developer-portal-cli-v2:latest
 export REPOSITORY=`docker run --rm \
   -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME \
   -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD \
-  quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola.ag-forecastio`
+  quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola keboola.ag-forecastio`
 docker tag keboola/forecastio-augmentation:latest $REPOSITORY:$TRAVIS_TAG
 docker tag keboola/forecastio-augmentation:latest $REPOSITORY:latest
 eval $(docker run --rm \
